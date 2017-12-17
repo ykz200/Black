376233c2cdb2fb32b89bd6385232f0007f81c0e7
kz yuan <yuankz888@gmail.com>
2017-12-17 23:55:32 +0800

    系统架构数据库调整

--- src/main/resources/demo.sql
+++ src/main/resources/demo.sql
@@ -1,13 +1,13 @@
 ﻿#
 # SQL Export
 # Created by Querious (1072)
-# Created: 23 November 2017 at 08:36:55 GMT+8
+# Created: 17 December 2017 at 23:53:09 GMT+8
 # Encoding: Unicode (UTF-8)
 #
 
 
 DROP DATABASE IF EXISTS `blacklist_test`;
-CREATE DATABASE `blacklist_test` DEFAULT CHARACTER SET latin1 DEFAULT COLLATE latin1_swedish_ci;
+CREATE DATABASE `blacklist_test` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
 USE `blacklist_test`;
 
 
@@ -22,10 +22,23 @@ CREATE TABLE `info` (
   `position` varchar(128) DEFAULT ' ',
   `companyName` varchar(128) DEFAULT ' ',
   `enable` varchar(1) DEFAULT 'Y',
-  `remarks` varchar(500) DEFAULT '单位性质培训 信息来源网络',
+  `remarks` varchar(300) DEFAULT '单位性质培训 信息来源网络',
   `create_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `email` varchar(50) DEFAULT NULL COMMENT '申请邮箱',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=436 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=444 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `keys` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `key_value` varchar(200) CHARACTER SET latin1 DEFAULT NULL COMMENT 'key value 秘钥值',
+  `key_type` int(11) DEFAULT '0' COMMENT 'key_type key的类型\n	1、对接黑名单接口',
+  `key_remark` varchar(500) CHARACTER SET latin1 DEFAULT NULL COMMENT '备注',
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'key创建时间',
+  `stop_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '停止时间 有效期',
+  `enable` varchar(1) CHARACTER SET latin1 DEFAULT 'Y' COMMENT 'Y 即为可用 N 则反之',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='key秘钥库';
 
 
 CREATE TABLE `notice` (
@@ -48,31 +61,6 @@ CREATE TABLE `page` (
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 
 
-CREATE TABLE `ssvpn` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `line` varchar(128) DEFAULT NULL,
-  `serverIp` varchar(128) DEFAULT NULL,
-  `port` varchar(128) DEFAULT NULL,
-  `password` varchar(128) DEFAULT NULL,
-  `encryption` varchar(128) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE `user_info` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(32) NOT NULL DEFAULT '' COMMENT '用户名',
-  `password` varchar(32) DEFAULT NULL COMMENT '密码',
-  `usertype` varchar(2) DEFAULT NULL COMMENT '用户类型',
-  `enabled` int(2) DEFAULT NULL COMMENT '是否可用',
-  `realname` varchar(32) DEFAULT NULL COMMENT '真实姓名',
-  `qq` varchar(14) DEFAULT NULL COMMENT 'QQ',
-  `email` varchar(100) DEFAULT NULL,
-  `tel` varchar(255) DEFAULT NULL COMMENT '联系电话',
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='用户信息表';
-
-
 
 
 SET FOREIGN_KEY_CHECKS = @PREVIOUS_FOREIGN_KEY_CHECKS;
@@ -84,25 +72,20 @@ SET FOREIGN_KEY_CHECKS = 0;
 
 LOCK TABLES `info` WRITE;
 ALTER TABLE `info` DISABLE KEYS;
-INSERT INTO `info` (`id`, `position`, `companyName`, `enable`, `remarks`, `create_date`) VALUES (1,'北京市海淀区苏州街55号名商大厦3楼	','北京汇瑞安网络科技有限公司','Y','单位性质培训 信息来源网络','2017-11-22 19:02:51');
-INSERT INTO `info` (`id`, `position`, `companyName`, `enable`, `remarks`, `create_date`) VALUES (2,'北京市海淀区苏州街55号名商大厦3层','天瑞地安网络科技有限公司北京分公司','Y','单位性质培训 信息来源网络','2017-11-22 19:02:51');
-INSERT INTO `info` (`id`, `position`, `companyName`, `enable`, `remarks`, `create_date`) VALUES (3,'北京市海淀区苏州街55号名商大厦3层	','北京嘉惠卓通科技有限公司	','Y','单位性质培训 信息来源网络','2017-11-22 19:02:51');
-INSERT INTO `info` (`id`, `position`, `companyName`, `enable`, `remarks`, `create_date`) VALUES (4,'北京市海淀区苏州街55号名商大厦3层	','北京瑞丰时代网络科技有限公司	','Y','单位性质培训 信息来源网络','2017-11-22 19:02:51');
-INSERT INTO `info` (`id`, `position`, `companyName`, `enable`, `remarks`, `create_date`) VALUES (5,'北京市海淀区苏州街55号名商大厦3层	','北京天利瑞安科技有限公司	','Y','单位性质培训 信息来源网络','2017-11-22 19:02:51');
-INSERT INTO `info` (`id`, `position`, `companyName`, `enable`, `remarks`, `create_date`) VALUES (6,'北京市海淀区善缘街1号4层2-325	','北京瑞通合力科技有限公司	','Y','单位性质培训 信息来源网络','2017-11-22 19:02:51');
-INSERT INTO `info` (`id`, `position`, `companyName`, `enable`, `remarks`, `create_date`) VALUES (286,' ','黑龙江滨才学院','Y','网络黑名单 信息来源网络','2017-11-22 19:02:51');
-INSERT INTO `info` (`id`, `position`, `companyName`, `enable`, `remarks`, `create_date`) VALUES (304,'北京市昌平区生命科学园珠江摩尔大厦6号楼2单元701室','北京睿一科技有限公司','Y','单位性质培训 信息来源网络','2017-11-22 19:02:51');
-INSERT INTO `info` (`id`, `position`, `companyName`, `enable`, `remarks`, `create_date`) VALUES (319,'北京市昌平区霍营街道黄平路和龙锦三街交叉口驿府商务酒店隔壁社区服务中心3楼','北京独创时代','Y','单位性质培训','2017-11-22 19:02:51');
-INSERT INTO `info` (`id`, `position`, `companyName`, `enable`, `remarks`, `create_date`) VALUES (384,'北京市海淀区上地东路1号盈创动力五层','北京华胜龙腾软件技术有限公司','Y','这家公司合同、社保找第三方（智联等）签订，长期拖欠离职、在职员工薪资，欠所有离职员工最少十几万，在职员工大部分时间每月只能发1700左右。内部使用的IBM Lotus软件没有经过授权，普加甘特图等也是破解的。老板吴亚群，电话18910360249。欠我1w左右薪资，拖欠了2年多，还没有还清。我电话18721937934，有任何虚假信息，欢迎骚扰！','2017-11-22 19:02:51');
+INSERT INTO `info` (`id`, `position`, `companyName`, `enable`, `remarks`, `create_date`, `email`) VALUES (1,'北京市海淀区苏州街55号名商大厦3楼	','北京汇瑞安网络科技有限公司','Y','单位性质培训 信息来源网络','2017-11-22 19:02:51',NULL);
 ALTER TABLE `info` ENABLE KEYS;
+UNLOCK TABLES;
+
+
+LOCK TABLES `keys` WRITE;
+ALTER TABLE `keys` DISABLE KEYS;
+INSERT INTO `keys` (`id`, `key_value`, `key_type`, `key_remark`, `create_date`, `stop_date`, `enable`) VALUES (1,'186B3CBB13BCCFB485838AF8AA3248',0,'1','2017-11-26 01:09:32','0000-00-00 00:00:00','Y');
+ALTER TABLE `keys` ENABLE KEYS;
 UNLOCK TABLES;
 
 
 LOCK TABLES `notice` WRITE;
 ALTER TABLE `notice` DISABLE KEYS;
-INSERT INTO `notice` (`id`, `title`, `enable`, `createDate`, `content`) VALUES (1,'IT黑目录第一篇公告','Y','2017-07-17 17:04:44','<h1>创建网站的第一感</h1>\n\n<p>在面试的这几天感觉当中，我感觉到了前所未有的困难。\n投出去的简历，回馈给我的却是十几家培训机构。\n这让我前所未有的恐惧，太可怕了。</p>\n\n<p>我心一想是不是所有IT新人都像我一样，找工作的阻挠都是这么多。</p>\n\n<p>我就每晚抽出一点时间，来做一个IT黑企的查询系统，数据来源网络。</p>\n\n<p>我开心我高兴至少付出是有回报的，既方便了我，也方便了大家。</p>\n\n<h3>为了方便交流</h3>\n\n<p>壳叔特此开放黑壳的交流群 <u>200408242</u> <b>OR</b> <a target="_blank" href="http://shang.qq.com/wpa/qunwpa?idkey=12017874d1dc78885dcad2e7e92e11174831d3cbc6d128e8be057eb828466f3a"><img border="0" src="//pub.idqqimg.com/wpa/images/group.png" alt="黑壳网技术交流" title="黑壳网技术交流"></a>\n用来反馈网站使用过程中产生的问题，并且大家都是程序员也可以方便沟通。</p>');
-INSERT INTO `notice` (`id`, `title`, `enable`, `createDate`, `content`) VALUES (11,'IT黑名单App V1.2版本发布','Y','2017-07-18 22:14:26','<h1>下载地址</h1>\n \n<p>下载地址 <a href="http://download.bhusk.com/ITBlackList_V1.2.apk" target="_blank">IT黑名单V1.2.apk</a>\n \n<br/>备用：<a href="https://bhusk99.ctfile.com/fs/12729178-212306412" target="_blank">IT黑名单V1.2.apk</a><br/></p>\n <br/><span>注：V1.0版本无法下载请浏览器打开</span>\n<br/>http://t.im/1f62y 进行版本更新\n<p><br/>更新内容\n<br/>1、修复邮箱链接无法分享问题。\n<br/>2、修复返回直接退出app。\n<br/>3、修复文件下载问题。\n<br/>4、修复无法调用QQ问题。\n<br/>5、修复诸多代码细节问题。');
-INSERT INTO `notice` (`id`, `title`, `enable`, `createDate`, `content`) VALUES (12,'IT黑名单网站代码GitHub开源','Y','2017-08-03 17:33:29','<p><br/>为了网站更加开源-已将代码上传。</p>\n\n<p>开源目的，你想改咋改~</p>\n\n<p><b>\n<a href="https://github.com/ykz200/Black">Github:https://github.com/ykz200/Black</a></b></p>');
 INSERT INTO `notice` (`id`, `title`, `enable`, `createDate`, `content`) VALUES (13,'【壳叔极力推荐】我在北京一家公司打工两个月，昨夜露宿街头','Y','2017-11-14 15:06:33','<p><br/>我在北京一家公司打工两个月，昨夜露宿街头</p>\n\n<p>壳叔极力推荐选择公司之前阅读文章</p>\n\n<p><b>\n<a href="https://juejin.im/post/5a0918856fb9a044fe45dacc">https://juejin.im/post/5a0918856fb9a044fe45dacc</a></b></p>');
 ALTER TABLE `notice` ENABLE KEYS;
 UNLOCK TABLES;
@@ -114,7 +97,7 @@ INSERT INTO `page` (`id`, `pageName`, `pageValue`, `remarks`, `enable`) VALUES (
 INSERT INTO `page` (`id`, `pageName`, `pageValue`, `remarks`, `enable`) VALUES (2,'js_map_index','<script>\n(function(){\n    var bp = document.createElement(\'script\');\n    var curProtocol = window.location.protocol.split(\':\')[0];\n    if (curProtocol === \'https\') {\n        bp.src = \'https://zz.bdstatic.com/linksubmit/push.js\';        \n    }\n    else {\n        bp.src = \'http://push.zhanzhang.baidu.com/push.js\';\n    }\n    var s = document.getElementsByTagName("script")[0];\n    s.parentNode.insertBefore(bp, s);\n})();\n</script>\n\n','移动端js动态添加','Y');
 INSERT INTO `page` (`id`, `pageName`, `pageValue`, `remarks`, `enable`) VALUES (3,'head_index',' ','pc端index 页面<head></head>之间内容','Y');
 INSERT INTO `page` (`id`, `pageName`, `pageValue`, `remarks`, `enable`) VALUES (4,'head_map_index',' ','移动端index 页面<head></head>之间内容','Y');
-INSERT INTO `page` (`id`, `pageName`, `pageValue`, `remarks`, `enable`) VALUES (5,'footer_pc','© Copyright 2017 北京IT黑名单 Inc.All Rights Reserved. <a href="http://www.miitbeian.gov.cn">京ICP备17029987号-2</a>\n\n','pc端尾部内容','Y');
+INSERT INTO `page` (`id`, `pageName`, `pageValue`, `remarks`, `enable`) VALUES (5,'footer_pc','© Copyright 2017 北京IT黑名单 Inc.All Rights Reserved. <a href="http://www.miitbeian.gov.cn"></a>\n\n','pc端尾部内容','Y');
 INSERT INTO `page` (`id`, `pageName`, `pageValue`, `remarks`, `enable`) VALUES (6,'footer_map','© Copyright 2017 北京IT黑名单 Inc.All Rights Reserved.\n<script src="https://s22.cnzz.com/z_stat.php?id=1262757961&web_id=1262757961" language="JavaScript"></script>\n','移动端尾部内容','Y');
 INSERT INTO `page` (`id`, `pageName`, `pageValue`, `remarks`, `enable`) VALUES (7,'header_index','<!--<span>如果有未收录的黑企信息，请将企业信息的以邮件的方式发送至\n    <a href="mailto:keshu@bhusk.com">keshu@bhusk.com</a>\n如使用过程中有问题请及时邮件反馈沟通，我们将尽快修复。</span>\n<br/><br/><br/><span style="color:red">公告功能上线:<a href="http://black.bhusk.com/notice">点击我看花样公告</a> -->','PC端主页头部提示内容','Y');
 INSERT INTO `page` (`id`, `pageName`, `pageValue`, `remarks`, `enable`) VALUES (8,'header_map_index','<span>如果有未收录的黑企信息，请将企业信息的以邮件的方式发送至\n    <a href="mailto:keshu@bhusk.com">keshu@bhusk.com</a>\n使用过程中有问题请及时邮件反馈沟通，我们将尽快修复。\n<br/><span style="color:red">公告功能上线:<a href="http://black.bhusk.com/notice">点击我看花样公告</a> \n<span style="color:#fff">\n<a target="_blank" href="http://shang.qq.com/wpa/qunwpa?idkey=25b503daf8d907bcb7cbcecd2ee512ef6cad4bd31f048bea7b0103a17397cf7a">官方 QQ群：200408242</a>\n</span>','移动端主页头部提示内容','Y');
@@ -131,23 +114,6 @@ INSERT INTO `page` (`id`, `pageName`, `pageValue`, `remarks`, `enable`) VALUES (
 INSERT INTO `page` (`id`, `pageName`, `pageValue`, `remarks`, `enable`) VALUES (19,'footer_advertising_index_map','<script src=\'http://veim.lrswl.com/s.php?id=5402\'></script>\n<script src=\'http://veim.lrswl.com/s.php?id=5929\'></script>','广告移动端index页面尾部','Y');
 INSERT INTO `page` (`id`, `pageName`, `pageValue`, `remarks`, `enable`) VALUES (20,'site_title_notice','公告栏-IT黑名单-精准查询it黑企业','公告标题','Y');
 ALTER TABLE `page` ENABLE KEYS;
-UNLOCK TABLES;
-
-
-LOCK TABLES `ssvpn` WRITE;
-ALTER TABLE `ssvpn` DISABLE KEYS;
-ALTER TABLE `ssvpn` ENABLE KEYS;
-UNLOCK TABLES;
-
-
-LOCK TABLES `user_info` WRITE;
-ALTER TABLE `user_info` DISABLE KEYS;
-INSERT INTO `user_info` (`Id`, `username`, `password`, `usertype`, `enabled`, `realname`, `qq`, `email`, `tel`) VALUES (1,'test1','12345678','1',NULL,NULL,NULL,NULL,NULL);
-INSERT INTO `user_info` (`Id`, `username`, `password`, `usertype`, `enabled`, `realname`, `qq`, `email`, `tel`) VALUES (2,'test2','aaaa','2',NULL,NULL,NULL,NULL,NULL);
-INSERT INTO `user_info` (`Id`, `username`, `password`, `usertype`, `enabled`, `realname`, `qq`, `email`, `tel`) VALUES (3,'test3','bbbb','1',NULL,NULL,NULL,NULL,NULL);
-INSERT INTO `user_info` (`Id`, `username`, `password`, `usertype`, `enabled`, `realname`, `qq`, `email`, `tel`) VALUES (4,'test4','cccc','2',NULL,NULL,NULL,NULL,NULL);
-INSERT INTO `user_info` (`Id`, `username`, `password`, `usertype`, `enabled`, `realname`, `qq`, `email`, `tel`) VALUES (5,'test5','dddd','1',NULL,NULL,NULL,NULL,NULL);
-ALTER TABLE `user_info` ENABLE KEYS;
 UNLOCK TABLES;
 
 
